a3a0535debc4cce7e35dcc8f9109ae44bcdbd622
DrewSpringham <drewspringham@gmail.com>
2022-06-28 18:42:07 +0100

    Attempting testing on real instances

---
+++ tests/real_instances.py
@@ -0,0 +1,25 @@
+import os
+
+from tqdm import tqdm
+
+from src.helpers import check_optimality
+from src.rules.interval_knapsack import interval_knapsack_projects
+from tests.pabulib_converter import convert_to_election
+
+
+def real_instances():
+    directory = "pb_files"
+    for filename in tqdm(os.listdir(directory)):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+
+            E = convert_to_election(f)
+            ik_result = interval_knapsack_projects(E)
+
+            if not check_optimality(E, ik_result):
+                raise ValueError(f"interval knapsack failed on: {f}")
+
+
+if __name__ == "__main__":
+    real_instances()
