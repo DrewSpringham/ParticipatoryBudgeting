d5cda94076d768e7b794122ffbe5cbaa1c41375a
DrewSpringham <drewspringham@gmail.com>
2022-06-28 17:49:48 +0100

    Proper conversion

--- tests/pabulib_converter.py
+++ tests/pabulib_converter.py
@@ -1,49 +1,53 @@
 from src.election_instance import Election
+from src.rules.interval_knapsack import interval_knapsack_projects
 from tests.random_instances import random_project
 
 
 def convert(filepath):
     meta = {}
-    project_ids = {}
+    projects = set()
     voters = set()
     phase = 0
     approvals = {}
-    projects = set()
+    project_ids = {}
     with open(filepath, encoding="utf8") as f:
-        lines = f.readlines()
+        lines = [l.strip() for l in f.readlines()]
 
-        for l in lines:
-            if phase == 0:
-                if
-                    if l != "PROJECTS\n":
-                        splitted = l.split(";")
-                        if len(splitted) == 2:
-                            meta[splitted[0]] = splitted[1]
-                    else:
-                        phase = 1
-            elif phase == 1:
-                if l != "VOTES\n":
-                    splitted = l.split(";")
-                    if splitted[0] != "project_id":
-                        cost = int(splitted[1])
-                        new_project = random_project(cost, cost)
-                        projects.add(new_project)
-                        project_ids[splitted[0]] = new_project
-                else:
-                    phase = 2
-            elif phase == 2:
-                splitted = l.split(";")
-                if splitted[0] != "voter_id":
-                    voters.add(splitted[0])
-                    votes = splitted[1].split(',')
-                    print(votes)
-                    cost = int(splitted[1])
-                    new_project = random_project(cost, cost)
-                    projects.add(new_project)
-                    project_ids[splitted[0]] = new_project
-    if meta["vote_type"] != "approval\n":
+        line_number = 2
+        while lines[line_number] != "PROJECTS":
+            l = lines[line_number].split(";")
+            meta[l[0]] = l[1]
+            line_number += 1
+
+        line_number += 1
+        l = lines[line_number].split(";")
+        id_index = l.index("project_id")
+        cost_index = l.index("cost")
+        line_number += 1
+        while lines[line_number] != "VOTES":
+            l = lines[line_number].split(";")
+            cost = int(l[cost_index])
+            p = random_project(cost, cost)
+            projects.add(p)
+            project_ids[l[id_index]] = p
+            line_number += 1
+
+        line_number += 1
+        l = lines[line_number].split(";")
+        id_index = l.index("voter_id")
+        vote_index = l.index("vote")
+        line_number += 1
+        for line in lines[line_number:]:
+            l = line.split(";")
+            voter = l[id_index]
+            votes = set(l[vote_index].split(","))
+            approval = set([project_ids[v] for v in votes])
+            approvals[voter] = approval
+            voters.add(voter)
+    if meta["vote_type"] != "approval":
         raise ValueError("Cannot operate on non-approval elections!")
-    return Election(voters, projects, approvals, meta["budget"])
+    return Election(voters, projects, approvals, int(meta["budget"]))
 
 
-convert("pb_files/poland_warszawa_2017_miedzeszyn.pb")
+E = convert("pb_files/poland_warszawa_2017_miedzeszyn.pb")
+print(interval_knapsack_projects(E))
