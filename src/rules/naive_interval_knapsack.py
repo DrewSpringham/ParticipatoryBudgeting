54f718f1f0e832361293599fc8292d32ce0ab95d
DrewSpringham <drewspringham@gmail.com>
2022-08-11 12:27:57 +0100

    Renamed the naive interval knapsack code to avoid name conflicts

--- src/rules/naive_interval_knapsack.py
+++ src/rules/naive_interval_knapsack.py
@@ -1,25 +1,25 @@
 from src.election_instance import Election
 
 
-def from_table(E, P, m, i, w):
+def naive_from_table(E, P, m, i, w):
     if i == 0:
         return set()
     wi = P[i - 1].cost
     if wi > w:
-        return from_table(E, P, m, i - 1, w)
+        return naive_from_table(E, P, m, i - 1, w)
     for k in range(i - 1, -2, -1):
         if P[k].end < P[i - 1].start:
             break
     k = k + 1
     if m[i - 1][w] > m[k][w - wi] + E.approvals_by_project[P[i - 1]]:
-        proj_set = from_table(E, P, m, i - 1, w)
+        proj_set = naive_from_table(E, P, m, i - 1, w)
     else:
-        proj_set = from_table(E, P, m, k, w - wi)
+        proj_set = naive_from_table(E, P, m, k, w - wi)
         proj_set.add(P[i - 1])
     return proj_set
 
 
-def interval_knapsack_table(E: Election):
+def naive_interval_knapsack_table(E: Election):
     n = len(E.projects)
     P = list(sorted(E.projects, key=lambda p: p.end))
     W = E.budget
@@ -39,17 +39,17 @@ def interval_knapsack_table(E: Election):
     return m
 
 
-def interval_knapsack_score(E: Election):
+def naive_interval_knapsack_score(E: Election):
     i = len(E.projects)
     W = E.budget
-    m = interval_knapsack_table(E)
+    m = naive_interval_knapsack_table(E)
     return m[i][W]
 
 
-def interval_knapsack_projects(E):
-    m = interval_knapsack_table(E)
+def naive_interval_knapsack_projects(E):
+    m = naive_interval_knapsack_table(E)
     i = len(E.projects)
     W = E.budget
     P = list(sorted(E.projects, key=lambda p: p.end))
-    proj_set = from_table(E, P, m, i, W)
+    proj_set = naive_from_table(E, P, m, i, W)
     return proj_set
