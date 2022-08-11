7f1cbd3b024ad1ae389f60a392225ef864d5d6f8
DrewSpringham <drewspringham@gmail.com>
2022-08-11 14:57:07 +0100

    Proper benchmarking, storing in excel sheet to export to external data analysis

--- tests/benchmarking.py
+++ tests/benchmarking.py
@@ -1,87 +1,68 @@
-import pickle
 import timeit
 
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy import stats
+import pandas as pd
+from openpyxl import load_workbook
 
 from src.rules.interval_knapsack import interval_knapsack_projects
+from src.rules.reverse_interval_knapsack import interval_knapsack_projects_reversed
+from tests.random_instances import random_instances
+from tests.real_instances import real_instances
 
 
-def generate():
-    count = 0
-    (xs, times, old_es) = pickle.load(open("C:/Users/Student/Documents/GitHub/PB/tests/data_reversed.pickle", "rb"))
-    times = []
-    xs = []
-    es = []
+def generate(data_source, rules):
+    elections = {'election_id': [], 'voters': [], 'projects': [], 'budget': []}
+    times = {'election_id': [], 'rule_id': [], 'time': []}
+    if data_source == 'random':
+        source = random_instances(10000, 100, 100000, 5, 50)
+    elif data_source == 'real':
+        source = real_instances()
+    else:
+        raise ValueError("Unknown data source!")
     try:
-        for E in old_es:
-
-            count += 1
-            start = timeit.default_timer()
-
-            ik_result = interval_knapsack_projects(E)
-            end = timeit.default_timer()
-            time = end - start
-            times.append(time)
-            es.append(E)
-            xs.append(len(E.projects) * len(E.projects) * E.budget)
-            if count % 10 == 0:
-                x = np.array(xs)
-                y = np.array(times)
-                gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-                mn = np.min(x)
-                mx = np.max(x)
-                x1 = np.linspace(mn, mx, 500)
-                y1 = gradient * x1 + intercept
-                plt.plot(x, y, 'xb')
-                plt.plot(x1, y1, '-r')
-
-                plt.xlabel(f"Number of projects squared times number of voters.")
-                plt.ylabel("Time (s)")
-                plt.title(
-                    "Number of projects squared times number of voters runtime\nof optimised knapsack algorithm.")
-                plt.show()
-
+        stop = False
+        print("Starting testing")
+        for E in source:
+            election_id = E.election_id
+            print(f"Testing election {election_id}")
+            elections['election_id'].append(election_id)
+            elections['voters'].append(len(E.voters))
+            elections['projects'].append(len(E.projects))
+            elections['budget'].append(E.budget)
+            for (rule, rule_id) in rules:
+                start = timeit.default_timer()
+                ik_result = rule(E)
+                end = timeit.default_timer()
+                time = end - start
+                print(f"Finished on rule {rule_id}")
+                times['election_id'].append(election_id)
+                times['rule_id'].append(rule_id)
+                times['time'].append(time)
+                """
+                if time>180:
+                    stop=True
+                    break
+                """
+            if stop:
+                break
+        raise KeyboardInterrupt
     except KeyboardInterrupt:
-        with open('data.pickle', 'wb') as handle:
-            pickle.dump((xs, times, es), handle, protocol=pickle.HIGHEST_PROTOCOL)
-        x = np.array(xs)
-        y = np.array(times)
-        gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-        mn = np.min(x)
-        mx = np.max(x)
-        x1 = np.linspace(mn, mx, 500)
-        y1 = gradient * x1 + intercept
-        plt.plot(x, y, 'ob')
-        plt.plot(x1, y1, '-r')
+        path = f"benchmarks_{data_source}.xlsx"
+        with pd.ExcelWriter(path, mode="a", if_sheet_exists='overlay', engine="openpyxl") as writer:
 
-        plt.xlabel(f"Number of projects squares times number of voters.\nr-value of {r_value}")
-        plt.ylabel("Time (s)")
-        plt.title(
-            "Number of projects squares times number of voters runtime\nof knapsack algorithm.")
-        plt.show()
-
-
-def load():
-    (xs, times, es) = pickle.load(open("data_reversed.pickle", "rb"))
-
-    x = np.array(xs)
-    y = np.array(times)
-    gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-    mn = np.min(x)
-    mx = np.max(x)
-    x1 = np.linspace(mn, mx, 500)
-    y1 = gradient * x1 + intercept
-    plt.plot(x, y, 'ob')
-    plt.plot(x1, y1, '-r')
-
-    plt.xlabel(f"Number of projects squares times number of voters.\nr-value of {r_value}")
-    plt.ylabel("Time (s)")
-    plt.title(
-        "Number of projects squares times number of voters runtime\nof knapsack algorithm.")
-    plt.show()
+            election_frame = pd.DataFrame(elections)
+            times_frame = pd.DataFrame(times)
+            book = load_workbook(path)
+            writer.book = book
+            writer.sheets = {ws.title: ws for ws in book.worksheets}
+            election_frame.to_excel(writer, startrow=writer.sheets['Elections'].max_row, header=False, index=False,
+                                    sheet_name="Elections")
+            times_frame.to_excel(writer, startrow=writer.sheets['Times'].max_row, header=False, index=False,
+                                 sheet_name="Times")
 
 
 if __name__ == "__main__":
-    generate()
+    generate('real', [
+        (interval_knapsack_projects_reversed, 2),
+        (interval_knapsack_projects, 1)  # ,
+        # (naive_interval_knapsack_projects, 0)
+    ])
