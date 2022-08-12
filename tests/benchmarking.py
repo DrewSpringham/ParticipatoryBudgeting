d1c3e0d5e51b5c8f18bf45a54bb61c04c9019d36
DrewSpringham <drewspringham@gmail.com>
2022-08-12 13:05:15 +0100

    Better benchmarking

--- tests/benchmarking.py
+++ tests/benchmarking.py
@@ -13,7 +13,7 @@ def generate(data_source, rules):
     elections = {'election_id': [], 'voters': [], 'projects': [], 'budget': []}
     times = {'election_id': [], 'rule_id': [], 'time': []}
     if data_source == 'random':
-        source = random_instances(10000, 100, 100000, 5, 50)
+        source = random_instances(10000, 1000, 300000, 3, 70)
     elif data_source == 'real':
         source = real_instances()
     else:
@@ -21,43 +21,60 @@ def generate(data_source, rules):
     try:
         stop = False
         print("Starting testing")
-        for E in source:
-            election_id = E.election_id
-            print(f"Testing election {election_id}")
-            elections['election_id'].append(election_id)
-            elections['voters'].append(len(E.voters))
-            elections['projects'].append(len(E.projects))
-            elections['budget'].append(E.budget)
-            for (rule, rule_id) in rules:
-                start = timeit.default_timer()
-                ik_result = rule(E)
-                end = timeit.default_timer()
-                time = end - start
-                print(f"Finished on rule {rule_id}")
-                times['election_id'].append(election_id)
-                times['rule_id'].append(rule_id)
-                times['time'].append(time)
-                """
-                if time>180:
-                    stop=True
-                    break
-                """
-            if stop:
-                break
+        for n, E in enumerate(source):
+            if n > 325:
+                election_id = E.election_id
+                print(f"Testing election {n}")
+                elections['election_id'].append(election_id)
+                elections['voters'].append(len(E.voters))
+                elections['projects'].append(len(E.projects))
+                elections['budget'].append(E.budget)
+                for (rule, rule_id) in rules:
+                    estimate = None
+                    if rule_id == 0:
+                        estimate = 4.911e-7 * len(E.projects) * E.budget
+                    elif rule_id == 1:
+                        estimate = 4.322e-8 * len(E.projects) * E.budget
+                    elif rule_id == 2:
+                        estimate = 2.327e-8 * len(E.voters) * len(E.projects) ** 2
+                    if estimate <= 600:
+                        start = timeit.default_timer()
+                        ik_result = rule(E)
+                        end = timeit.default_timer()
+                        time = end - start
+                        print(f"Finished on rule {rule_id}")
+                        times['election_id'].append(election_id)
+                        times['rule_id'].append(rule_id)
+                        times['time'].append(time)
         raise KeyboardInterrupt
-    except KeyboardInterrupt:
-        path = f"benchmarks_{data_source}.xlsx"
-        with pd.ExcelWriter(path, mode="a", if_sheet_exists='overlay', engine="openpyxl") as writer:
+    except:
+        try:
+            path = f"benchmarks_{data_source}.xlsx"
+            with pd.ExcelWriter(path, mode="a", if_sheet_exists='overlay', engine="openpyxl") as writer:
 
-            election_frame = pd.DataFrame(elections)
-            times_frame = pd.DataFrame(times)
-            book = load_workbook(path)
-            writer.book = book
-            writer.sheets = {ws.title: ws for ws in book.worksheets}
-            election_frame.to_excel(writer, startrow=writer.sheets['Elections'].max_row, header=False, index=False,
-                                    sheet_name="Elections")
-            times_frame.to_excel(writer, startrow=writer.sheets['Times'].max_row, header=False, index=False,
-                                 sheet_name="Times")
+                election_frame = pd.DataFrame(elections)
+                times_frame = pd.DataFrame(times)
+                book = load_workbook(path)
+                writer.book = book
+                writer.sheets = {ws.title: ws for ws in book.worksheets}
+                election_frame.to_excel(writer, startrow=writer.sheets['Elections'].max_row, header=False, index=False,
+                                        sheet_name="Elections")
+                times_frame.to_excel(writer, startrow=writer.sheets['Times'].max_row, header=False, index=False,
+                                     sheet_name="Times")
+        except PermissionError:
+            t = timeit.default_timer()
+            path = f"benchmarks_{data_source}_{t}.xlsx"
+            with pd.ExcelWriter(path, if_sheet_exists='overlay', engine="openpyxl") as writer:
+
+                election_frame = pd.DataFrame(elections)
+                times_frame = pd.DataFrame(times)
+                book = load_workbook(path)
+                writer.book = book
+                writer.sheets = {ws.title: ws for ws in book.worksheets}
+                election_frame.to_excel(writer, startrow=writer.sheets['Elections'].max_row, header=False, index=False,
+                                        sheet_name="Elections")
+                times_frame.to_excel(writer, startrow=writer.sheets['Times'].max_row, header=False, index=False,
+                                     sheet_name="Times")
 
 
 if __name__ == "__main__":
