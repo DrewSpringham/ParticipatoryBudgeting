35e5a39f1e3b69115c8c1d43746c460aaa04eb21
DrewSpringham <drewspringham@gmail.com>
2022-08-13 11:02:45 +0100

    Small changes to benchmarking

--- tests/benchmarking.py
+++ tests/benchmarking.py
@@ -13,7 +13,7 @@ def generate(data_source, rules):
     elections = {'election_id': [], 'voters': [], 'projects': [], 'budget': []}
     times = {'election_id': [], 'rule_id': [], 'time': []}
     if data_source == 'random':
-        source = random_instances(10000, 1000, 300000, 3, 70)
+        source = random_instances(50, 1000, 300000, 3, 70)
     elif data_source == 'real':
         source = real_instances()
     else:
@@ -22,30 +22,30 @@ def generate(data_source, rules):
         stop = False
         print("Starting testing")
         for n, E in enumerate(source):
-            if n > 325:
-                election_id = E.election_id
-                print(f"Testing election {n}")
-                elections['election_id'].append(election_id)
-                elections['voters'].append(len(E.voters))
-                elections['projects'].append(len(E.projects))
-                elections['budget'].append(E.budget)
-                for (rule, rule_id) in rules:
-                    estimate = None
-                    if rule_id == 0:
-                        estimate = 4.911e-7 * len(E.projects) * E.budget
-                    elif rule_id == 1:
-                        estimate = 4.322e-8 * len(E.projects) * E.budget
-                    elif rule_id == 2:
-                        estimate = 2.327e-8 * len(E.voters) * len(E.projects) ** 2
-                    if estimate <= 600:
-                        start = timeit.default_timer()
-                        ik_result = rule(E)
-                        end = timeit.default_timer()
-                        time = end - start
-                        print(f"Finished on rule {rule_id}")
-                        times['election_id'].append(election_id)
-                        times['rule_id'].append(rule_id)
-                        times['time'].append(time)
+
+            election_id = E.election_id
+            print(f"Testing election {n}")
+            elections['election_id'].append(election_id)
+            elections['voters'].append(len(E.voters))
+            elections['projects'].append(len(E.projects))
+            elections['budget'].append(E.budget)
+            for (rule, rule_id) in rules:
+                estimate = None
+                if rule_id == 0:
+                    estimate = 4.911e-7 * len(E.projects) * E.budget
+                elif rule_id == 1:
+                    estimate = 4.322e-8 * len(E.projects) * E.budget
+                elif rule_id == 2:
+                    estimate = 2.327e-8 * len(E.voters) * len(E.projects) ** 2
+                if estimate <= 600:
+                    start = timeit.default_timer()
+                    ik_result = rule(E)
+                    end = timeit.default_timer()
+                    time = end - start
+                    print(f"Finished on rule {rule_id}")
+                    times['election_id'].append(election_id)
+                    times['rule_id'].append(rule_id)
+                    times['time'].append(time)
         raise KeyboardInterrupt
     except:
         try:
@@ -78,8 +78,9 @@ def generate(data_source, rules):
 
 
 if __name__ == "__main__":
-    generate('real', [
-        (interval_knapsack_projects_reversed, 2),
-        (interval_knapsack_projects, 1)  # ,
-        # (naive_interval_knapsack_projects, 0)
-    ])
+    while True:
+        generate('random', [
+            (interval_knapsack_projects_reversed, 2),
+            (interval_knapsack_projects, 1)  # ,
+            # (naive_interval_knapsack_projects, 0)
+        ])
